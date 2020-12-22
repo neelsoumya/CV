dc2ff886a3651ab6b7ce1794eb7a2a29e5abcbf9
Soumya Banerjee <neel.soumya@gmail.com>
2020-12-22 10:04:40 +0000

    Update sortbibfile.R

--- sortbibfile.R
+++ sortbibfile.R
@@ -6,6 +6,6 @@ for (i in seq_along(rjh)) {
   rjh[[i]]$abstract <- NULL
   rjh[[i]]$keywords <- NULL
 }
-WriteBib(sort(rjh, sorting = "ydnt"), "rjhpubs.bib")
+WriteBib(sort(rjh, sorting = "ydnt"), "all_my_papers_ref.bib")
 # Beware: Carta2011 authors get screwed up by RefManageR
 # Replace with author = {Davide Carta and Laura Villanova and Stefano Costacurta and Alessandro Patelli and Irene Poli and Simone Vezzù and Paolo Scopece and Fabio Lisi and Kate Smith-Miles and Rob J Hyndman and Anita J Hill and Paolo Falcaro},
